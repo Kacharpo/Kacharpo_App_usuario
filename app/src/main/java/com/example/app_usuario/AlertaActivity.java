52ce7c8a5fad96990c3a130853fc24ca259b45b1
antonyjuarez <antonybrowm@gmail.com>
2021-07-11 01:50:22 -0500

    Se añadio accion de boton para cambiar de act. Primera Entr

--- app/src/main/java/com/example/app_usuario/AlertaActivity.java
+++ app/src/main/java/com/example/app_usuario/AlertaActivity.java
@@ -2,7 +2,10 @@ package com.example.app_usuario;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AlertaActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class AlertaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alerta2);
+
+        Button info = findViewById(R.id.BtnMensajeAlerta);
+        info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), DisponibilidadActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
     }
 }
